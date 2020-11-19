7dc48f0ff942823f496b136792bbb060d2c4d5d8
abel <abelpinheiro95@gmail.com>
2020-11-19 10:20:14 -0300

    Criar rotas home e /questions

---
+++ src/app/views/question-crud/question-crud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-question-crud',
+  templateUrl: './question-crud.component.html',
+  styleUrls: ['./question-crud.component.css']
+})
+export class QuestionCrudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
